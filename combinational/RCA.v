b0267822a1bb7c614720d06f3319fa4c4bafe9be
UIT 25521672 <hpnt1912@gmail.com>
2026-05-14 11:33:50 +0700

    Create RCA.v
    add RCA 4 bit

---
+++ combinational/RCA.v
@@ -0,0 +1,16 @@
+module RCA(
+    input [3:0] a,
+    input [3:0] b,
+    input cin,
+    output [3:0] s,
+    output cout
+);
+
+wire c1, c2, c3;
+
+full_adder FA0(.a(a[0]), .b(b[0]), .cin(cin), .s(s[0]), .cout(c1));
+full_adder FA1(.a(a[1]), .b(b[1]), .cin(c1), .s(s[1]), .cout(c2));
+full_adder FA2(.a(a[2]), .b(b[2]), .cin(c2), .s(s[2]), .cout(c3));
+full_adder FA3(.a(a[3]), .b(b[3]), .cin(c3), .s(s[3]), .cout(cout));
+
+endmodule
